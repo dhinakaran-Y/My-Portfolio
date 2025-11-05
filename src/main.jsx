284ea85225f446e2678dbff5e45314bbcd66e37a
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2025-11-05 12:22:54 +0530

    init-2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='font-bold text-4xl text-red-500 m-12 underline'>hii, Dhina</h1>
+    <h1 className='font-bold text-4xl text-red-500 m-12 underline'>hello, Dhina</h1>
     <App />
   </StrictMode>,
 )
